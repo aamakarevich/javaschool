a19193a2f368fdf569ffbe37b2e897965ee969f7
Andrei Makarevich <aamakarevich@mail.ru>
2016-06-19 04:42:38 +0300

    Options managements stuff

--- src/main/java/com/tsystems/ecare/controllers/FeatureServlet.java
+++ src/main/java/com/tsystems/ecare/controllers/FeatureServlet.java
@@ -17,12 +17,6 @@ import java.io.IOException;
 public class FeatureServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        JsonUtil.writeObjectToJson(resp, new FeatureServiceImpl().getAllFeatures());
-    }
-
-    @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         Integer idToDelete = Integer.parseInt(req.getPathInfo().replace("/", ""));
@@ -36,5 +30,33 @@ public class FeatureServlet extends HttpServlet {
             }
         }
         resp.getWriter().write("fail");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        if (!req.getPathInfo().equals("/")) {
+            Integer idToGet = Integer.parseInt(req.getPathInfo().replace("/", ""));
+            Feature feature = new FeatureServiceImpl().getFeature(idToGet);
+            JsonUtil.writeObjectToJson(resp, feature);
+            return;
+        }
+
+        JsonUtil.writeObjectToJson(resp, new FeatureServiceImpl().getAllFeatures());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        Feature feature = JsonUtil.getObjectFromJson(req, Feature.class);
+        new FeatureServiceImpl().saveNewFeature(feature);
+        resp.setStatus(HttpServletResponse.SC_CREATED);
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Feature feature = JsonUtil.getObjectFromJson(req, Feature.class);
+        new FeatureServiceImpl().udpateFeature(feature);
+        resp.setStatus(HttpServletResponse.SC_OK);
     }
 }
